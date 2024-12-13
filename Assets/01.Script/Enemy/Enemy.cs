3b7ee35c7e2f809828bd4cad6cc0885ed37f7338
Crogen0407 <mymcoding0407@gmail.com>
2024-12-13 09:46:05 +0900

    feat : Visual flip

--- Assets/01.Script/Enemy/Enemy.cs
+++ Assets/01.Script/Enemy/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent _navmeshAgent;
     public bool isStopped { get => _navmeshAgent.remainingDistance <= _navmeshAgent.stoppingDistance; }
     public Transform CurrentTarget { get; set; }
-
+    [SerializeField] private Transform _visualTrm;
     public Vector2 SelfAreaPosition { get; private set; }
     public float selfAreaRadius = 15.0f;
 
@@ -20,12 +20,18 @@ public class Enemy : MonoBehaviour
         SelfAreaPosition = transform.position;
     }
 
+    public void VisualFlip(bool flip)
+    {
+        _visualTrm.localScale = new Vector3(flip ? -1 : 1, 1, 1);
+    }
+    
     public void Move()
     {
         SetDestination(CurrentTarget.position);
     }
     public void SetDestination(Vector3 destination)
     {
+        VisualFlip(destination.x < transform.position.x);
         _navmeshAgent.destination = destination;
     }
 
